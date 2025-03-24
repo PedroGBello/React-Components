cb5c87859558937ce948e7efc7895903d0e0dabe
Pedro G. Bello <soy.pedrobello99@gmail.com>
2025-03-23 21:44:45 -0300

    add product image, update product data structure, and enhance styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,129 +1,164 @@
-import "./App.css";
-import picture from "./assets/product_1.webp";
+import { data } from "../src/data/data";
+import styles from "./App.module.css";
+
+import {
+	IoMdStar,
+	IoMdStarHalf,
+	IoMdStarOutline,
+	IoMdHeartEmpty,
+} from "react-icons/io";
 
 function App() {
+	const rating = data[0].rating.rating_stars / 10;
+	const fullStars = Math.floor(rating);
+	const hasHalfStar = rating % 1 !== 0;
+	const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
+
+	const discount = (
+		((data[0].previous_cents - data[0].current_cents) * 100) /
+		data[0].previous_cents
+	).toFixed();
+
 	return (
 		<>
-			<div className="ui-search-result__wrapper">
-				<div className="poly-card poly-card--grid-card card--flat card--animated">
-					<div className="poly-card__front">
+			<div className={styles["ui-search-result__wrapper"]}>
+				<div
+					className={[
+						styles["poly-card"],
+						styles["poly-card--grid-card"],
+						styles["card--flat"],
+						styles["card--animated"],
+					].join(" ")}
+				>
+					<div className={styles["poly-card__front"]}>
 						<img
-							src={picture}
+							src={data[0].images[0]}
 							alt=""
-							className="poly-component__picture"
+							className={styles["poly-component__picture"]}
 						/>
 					</div>
-					<div className="poly-card__content">
-						<h3 className="poly-component__title-wrapper">
+					<div className={styles["poly-card__content"]}>
+						<h3 className={styles["poly-component__title-wrapper"]}>
 							<a
 								href="https://www.mercadolibre.com.ar/rack-organizador-de-cocina-65cm-escurridor-seca-platos-color-negro/p/MLA37218388#polycard_client=search-nordic&searchVariation=MLA37218388&wid=MLA1993005910&position=27&search_layout=grid&type=product&tracking_id=9c052743-09aa-4dd3-8b3f-f272c920fb63&sid=search"
-								className="poly-component__title"
+								className={styles["poly-component__title"]}
 							>
-								Rack Organizador De Cocina 65cm Escurridor Seca
-								Platos Color Negro
+								{data[0].product_name}
 							</a>
 						</h3>
-						<div className="poly-component__reviews">
-							<span>4.5</span>
-							<span className="poly-reviews__stars">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									height="20px"
-									viewBox="0 -960 960 960"
-									width="20px"
-									fill="#3483FA"
-								>
-									<path d="m293-203.08 49.62-212.54-164.93-142.84 217.23-18.85L480-777.69l85.08 200.38 217.23 18.85-164.93 142.84L667-203.08 480-315.92 293-203.08Z" />
-								</svg>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									height="20px"
-									viewBox="0 -960 960 960"
-									width="20px"
-									fill="#3483FA"
-								>
-									<path d="m293-203.08 49.62-212.54-164.93-142.84 217.23-18.85L480-777.69l85.08 200.38 217.23 18.85-164.93 142.84L667-203.08 480-315.92 293-203.08Z" />
-								</svg>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									height="20px"
-									viewBox="0 -960 960 960"
-									width="20px"
-									fill="#3483FA"
-								>
-									<path d="m293-203.08 49.62-212.54-164.93-142.84 217.23-18.85L480-777.69l85.08 200.38 217.23 18.85-164.93 142.84L667-203.08 480-315.92 293-203.08Z" />
-								</svg>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									height="20px"
-									viewBox="0 -960 960 960"
-									width="20px"
-									fill="#3483FA"
-								>
-									<path d="m293-203.08 49.62-212.54-164.93-142.84 217.23-18.85L480-777.69l85.08 200.38 217.23 18.85-164.93 142.84L667-203.08 480-315.92 293-203.08Z" />
-								</svg>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									height="20px"
-									viewBox="0 -960 960 960"
-									width="20px"
-									fill="#3483FA"
-								>
-									<path d="m620.08-266.31-36.62-160.31 123.39-106.92L544.08-548 480-698.31v346.23l140.08 85.77ZM293-203.08l49.62-212.54-164.93-142.84 217.23-18.85L480-777.69l85.08 200.38 217.23 18.85-164.93 142.84L667-203.08 480-315.92 293-203.08Z" />
-								</svg>
+						<div className={styles["poly-component__reviews"]}>
+							<span>{rating.toFixed(1)}</span>
+							<span className={styles["poly-reviews__stars"]}>
+								{Array.from({
+									length: fullStars,
+								}).map((_, index) => (
+									<IoMdStar
+										key={index}
+										className={styles["star-icon"]}
+									/>
+								))}
+								{hasHalfStar && (
+									<IoMdStarHalf
+										className={styles["star-icon"]}
+									/>
+								)}
+								{Array.from({
+									length: emptyStars,
+								}).map((_, index) => (
+									<IoMdStarOutline
+										key={index}
+										className={styles["star-icon"]}
+									/>
+								))}
 							</span>
-							<span>(331)</span>
+							<span>({data[0].rating.rating_quantity})</span>
 						</div>
-						<div className="poly-component__price">
-							<s className="money-amount__previous">
-								<span className="money-amount__currency-symbol">
+						<div className={styles["poly-component__price"]}>
+							<s className={styles["money-amount__previous"]}>
+								<span
+									className={
+										styles["money-amount__currency-symbol"]
+									}
+								>
 									$
 								</span>
-								<span className="money-amount__fraction">
-									60.000
+								<span
+									className={styles["money-amount__fraction"]}
+								>
+									{data[0].previous_cents.toLocaleString(
+										"es-AR"
+									)}
 								</span>
 							</s>
-							<div className="poly-price__current">
-								<span className="money-amount">
-									<span className="money-amount__currency-symbol">
+							<div className={styles["poly-price__current"]}>
+								<span className={styles["money-amount"]}>
+									<span
+										className={
+											styles[
+												"money-amount__currency-symbol"
+											]
+										}
+									>
 										$
 									</span>
-									<span className="money-amount__fraction">
-										43.068
+									<span
+										className={
+											styles["money-amount__fraction"]
+										}
+									>
+										{data[0].current_cents.toLocaleString(
+											"es-AR"
+										)}
 									</span>
 								</span>
-								<span className="money-amount__discount">
-									28% OFF
-								</span>
+								{data[0].has_discount && (
+									<span
+										className={
+											styles["money-amount__discount"]
+										}
+									>
+										{discount}% OFF
+									</span>
+								)}
 							</div>
-							<span className="poly-price__installment">
-								en 6 cuotas de{" "}
-								<span className="money-amount">
-									<span className="money-amount__currency-symbol">
+							<span className={styles["poly-price__installment"]}>
+								en {data[0].installments[0]} cuotas de{" "}
+								<span className={styles["money-amount"]}>
+									<span
+										className={
+											styles[
+												"money-amount__currency-symbol"
+											]
+										}
+									>
 										$
 									</span>
-									<span className="money-amount__fraction">
-										9.543
+									<span
+										className={
+											styles["money-amount__fraction"]
+										}
+									>
+										{data[0].installments[1].toLocaleString(
+											"es-AR"
+										)}
 									</span>
 								</span>
 							</span>
 						</div>
-						<strong className="poly-component__shipping">
-							Envío gratis
-						</strong>
-					</div>
-					<div className="poly-component__bookmark">
-						<button className="poly-bookmark__btn" role="switch">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								height="24px"
-								viewBox="0 -960 960 960"
-								width="24px"
-								fill="#3483FA"
-								className="poly-bookmark__icon-empty"
+						{data[0].is_free_shipping && (
+							<strong
+								className={styles["poly-component__shipping"]}
 							>
-								<path d="m480-178-30-29q-105-97-171-162T177-480q-36-46-48.5-82.5T116-637q0-77 55.5-132T305-824q46 0 92 25t83 73q37-48 83-73t92-25q78 0 133.5 55T844-637q0 38-12 73.5t-48 81Q748-437 682-371T508-205l-28 27Zm0-63q95-87 156-147t97.5-104.5q36.5-44.5 50.5-78t14-66.5q0-60-40.5-100.5T655-778q-34 0-66 16t-66 51l-35 41h-16l-35-41q-35-36-67.5-51.5T305-778q-61 0-102 40.5T162-637q0 33 13 65t48.5 76q35.5 44 97 104T480-241Zm0-269Z" />
-							</svg>
+								Envío gratis
+							</strong>
+						)}
+					</div>
+					<div className={styles["poly-component__bookmark"]}>
+						<button
+							className={styles["poly-bookmark__btn"]}
+							role="switch"
+						>
+							<IoMdHeartEmpty className={styles["heart-icon"]} />
 						</button>
 					</div>
 				</div>
--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,18 @@
-data = {
-	products: [{ id: 1, name: "", price: 39899 }];
-}
+export const data = [
+	{
+		id: "0123456789",
+		images: ["../../public/assets/product_1.webp"],
+		product_name:
+			"Rack Organizador De Cocina 65cm Escurridor Seca Platos Color Negro",
+		rating: {
+			rating_stars: 45,
+			rating_quantity: 337,
+		},
+		currency: "ARS",
+		current_cents: 43068,
+		previous_cents: 60000,
+		has_discount: true,
+		installments: [6, 9543], // [cantidad de cuotas, monto]
+		is_free_shipping: true,
+	},
+];
